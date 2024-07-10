32a8a79eada2f511ca48555d9f4a570ab19c1790
Hirushiharan Thevendran <hirushiharant@gmail.com>
2024-07-10 13:32:02 +0530

    add api to get grid data

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ import uvicorn
 import json
 from pathlib import Path
 
-
 app = FastAPI()
 app.mount("/data", StaticFiles(directory="data"), name="data")
+
+# To check if the app is running
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
 
 @app.get("/sunburstData", response_class=JSONResponse)
 async def index(request: Request):
@@ -21,10 +25,17 @@ async def index(request: Request):
     else:
         return JSONResponse(content={"error": "File not found"}, status_code=404)
 
-# To check if the app is running
-@app.get("/")
-async def root():
-    return {"message": "Hello, World!"}
-
+@app.get("/gridData", response_class=JSONResponse)
+async def index(request: Request):
+    # Reading the JSON file
+    file_path = Path('data/gridData.json')
+    if file_path.exists():
+        with file_path.open('r') as file:
+            data = json.load(file)
+        print('Request for Sunburst Chart Data received')
+        return JSONResponse(content=data)
+    else:
+        return JSONResponse(content={"error": "File not found"}, status_code=404)
+    
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0', port=8000)
